9fc1f71d41fdd1a554d42ce0eca2678528543c88
fvgarcia93 <fv.garcia21@info.uas.edu.mx>
2026-06-26 15:56:49 -0700

    Add files via upload

---
+++ Controllers/TareasController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagerAPI.Data;
+using TaskManagerAPI.Models;
+
+namespace TaskManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TareasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Tarea>>> GetTareas()
+        {
+            return await _context.Tareas.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Tarea>> PostTarea(Tarea tarea)
+        {
+            _context.Tareas.Add(tarea);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTareas),
+                new { id = tarea.Id }, tarea);
+        }
+    }
+}
---
+++ Migrations/20260626221450_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tareas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Completada = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tareas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tareas");
+        }
+    }
+}
